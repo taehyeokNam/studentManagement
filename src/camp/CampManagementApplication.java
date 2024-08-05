c8ecda990835977a110f510297cd0f813df314d3
navyth91 <nayth91@naver.com>
2024-08-05 21:05:44 +0900

    수강생 과목별 회차 조회_수정

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -350,8 +350,13 @@ public class CampManagementApplication {
         newScore.setSubjectId(subjectId);
 
         String subjectName = "";
+        String subjectType = "";
+
         for(Subject subject : subjectStore) {
-            if(subject.getSubjectId().equals(subjectId)) subjectName = subject.getSubjectName();
+            if(subject.getSubjectId().equals(subjectId)) {
+                subjectName = subject.getSubjectName();
+                subjectType = subject.getSubjectType();
+            }
         }
         // 회차 입력
         int round = getRoundId();
@@ -360,10 +365,9 @@ public class CampManagementApplication {
         // 점수 입력
         int score = inputScore();
         newScore.setScore(score);
+        newScore.setGrade(subjectType);
 
         System.out.println("시험 점수를 등록합니다...");
-
-
 
         // 기능 구현
         scoreStore.add(newScore);
@@ -405,22 +409,22 @@ public class CampManagementApplication {
     private static void inquireRoundGradeBySubject() throws BadException {
         String studentId = getStudentId();                  // 관리할 수강생 고유 번호
         String studentSubject = getSubjectId();             // 관리할 과목 고유 번호
-        int round = getRoundId();                           // 관리할 회자 번호
+        int round = 1;
         char grade = ' ';                                   // 등급 저장 변수
-
-        if (round < 0) { throw new BadException("roundRangeError");}
 
         for (Score score : scoreStore) {
             if (score.getStudentId().equals(studentId) &&
-                    score.getSubjectId().equals(studentSubject)) { grade = score.getGrade();}
-        }
+                    score.getSubjectId().equals(studentSubject)) {
+                grade = score.getGrade();
 
-        // 기능 구현 (조회할 특정 과목)
-        System.out.println("회차별 등급을 조회합니다...");
-        System.out.println("==============================");
-        System.out.println("회차 = " + round);
-        System.out.println("등급 = " + grade);
-        System.out.println("==============================");
+                // 기능 구현 (조회할 특정 과목)
+                System.out.println("회차별 등급을 조회합니다...");
+                System.out.println("==============================");
+                System.out.println("회차 = " + round++);
+                System.out.println("등급 = " + grade);
+                System.out.println("==============================");
+            }
+        }
 
         // 기능 구현
         System.out.println("\n등급 조회 성공!");
