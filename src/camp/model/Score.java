2bb23f1addf01051a320511b7f16a789e6956875
nametaehyeok <navyth91@naver.com>
2024-08-05 15:24:54 +0900

    setGrade

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -4,8 +4,11 @@ public class Score {
     private String scoreId;
     private String studentId;
     private String subjectId;
-    private double score;
+    private int score;
     private int round;
+    private String subjectType;
+    private char grade;
+
 
     public Score(String seq) {
         this.scoreId = seq;
@@ -22,13 +25,16 @@ public class Score {
 
     public int getRound() { return round; }
 
-    public double getScore() {
+    public int getScore() {
         return score;
     }
 
+    public char getGrade() {return grade; }
+
+
     // Setter
 
-    public void setScore(double score) {
+    public void setScore(int score) {
         this.score = score;
     }
 
@@ -42,5 +48,27 @@ public class Score {
 
     public void setSubjectId(String subjectId) {
         this.subjectId = subjectId;
+    }
+
+    public void setGrade (String subjectType) {
+        switch (subjectType) {
+            case "MANDATORY" :
+                if (score > 94 && score <= 100) grade = 'A';
+                else if (score >89 && score <= 94) grade = 'B';
+                else if (score > 79 && score <= 89) grade = 'C';
+                else if (score > 69 && score <= 79) grade = 'D';
+                else if (score > 59 && score <= 69) grade = 'F';
+                else grade = 'N';
+                break;
+            case "CHOICE" :
+                if (score > 89 && score <= 100) grade = 'A';
+                else if (score >79 && score <= 89) grade = 'B';
+                else if (score > 69 && score <= 79) grade = 'C';
+                else if (score > 59 && score <= 69) grade = 'D';
+                else if (score > 49 && score <= 59) grade = 'F';
+                else grade = 'N';
+                break;
+            default:
+        }
     }
 }
