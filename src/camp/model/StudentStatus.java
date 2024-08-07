2d6b2dfc625f7ac4e8eacaa698d64889bb5d2563
jhwook <jhwooook@naver.com>
2024-08-07 15:47:29 +0900

    StudentStatus수정

--- src/camp/model/StudentStatus.java
+++ src/camp/model/StudentStatus.java
@@ -1,16 +1,7 @@
 package camp.model;
 
 public enum StudentStatus {
-    Green(1),
-    Yellow(2),
-    Red(3);
-    private final int num;
-
-    StudentStatus(int i) {
-        this.num = i;
-    }
-
-    public int getName() {
-        return num;
-    }
+    Green,
+    Yellow,
+    Red;
 }
